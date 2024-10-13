2e91f21b5936b808dfa7ab61bab7d036b11333d3
preeti28dec <79787988+preeti28dec@users.noreply.github.com>
2024-10-13 23:49:00 +0530

    Update header.jsx

--- src/componets/header.jsx
+++ src/componets/header.jsx
@@ -4,17 +4,19 @@ import { Links } from "./hamburger";
 function Header() {
   return (
     <section className="border-b-2 mx-auto main_header  ">
+      <div className=" sticky top-0">
       <div className="flex justify-between item-center items-center main_header1 w-10/12 mx-auto">
         <div className="rounded-lg px-4 flex gap-3 items-center">
           <div className="flex items-center">
-            <div className="mt-2 mx-2 w-[50%]">
-              <img src="../images/Infinytics2.svg" alt="logo-error" />
+            <div className="mt-2 mx-2">
+              <img src="../images/Infinytics2.svg"  class="h-[50px] object-contain" alt="logo-error" />
             </div>
           </div>
         </div>
         <div className="links flex gap-10 font-[400px] text-[18px] font-Quicksand ">
           <Links />
         </div>
+      </div>
       </div>
     </section>
   );
